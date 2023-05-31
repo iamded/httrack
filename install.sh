87d1b5bddc6a637c75386ccdb0d49e1a106f0556
iamded <dedetmasri@gmail.com>
2023-05-31 09:40:41 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,16 @@
+echo "Dont forget subcribe my chanel: iam ded !!!..."
+sleep 3
+termux-open-url https://youtube.com/channel/UCk0IHP3kOrqN86sXjs1c_Xw
+clear
+sleep 3
+echo "Checking package harap menunggu ya bos..!!!"
+sleep 3
+pkg update -y
+pkg upgrade -y
+pkg install bash git -y
+pkg install curl wget -y
+clear
+sleep 5
+curl -LO https://raw.githubusercontent.com/Hax4us/httrack_In_termux/master/httrack
+clear
+sleep 3
